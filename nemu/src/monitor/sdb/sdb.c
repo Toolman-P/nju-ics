3666df68c6d2575b34d8b3cb3278d3d869044d9e
tracer-ics2021 <tracer@njuics.org>
2022-05-07 13:48:46 +0800

    >  compile 201220000  Static hostname: epc        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: e2c4b6752e1c4f04b85873f26ef5a7ff          Boot ID: 2b1740e1c8814d80a436c686e5b67e4d Operating System: Manjaro Linux           Kernel: Linux 5.15.32-1-MANJARO     Architecture: x86-64  Hardware Vendor: Lenovo   Hardware Model: Lenovo Legion R9000P2021H  13:48:46 up  2:59,  2 users,  load average: 1.74, 1.33, 1.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -87,10 +87,10 @@ static int cmd_x(char *args){
   
   printf("---------MEMORY----------\n");
   printf("Total Bytes: %d\n",bytes);
-  printf("Starting Addr: 0X%016lx\n",addr);
+  printf("Starting Addr: 0x%016lx\n",addr);
   printf("Mem: ");
   for(int i=0;i<bytes;i++)
-      printf("%x ",*guest_to_host(addr+i));
+      printf("0x%02x ",*guest_to_host(addr+i));
   printf("\n");
   printf("-----------END----------\n");
   return 0;

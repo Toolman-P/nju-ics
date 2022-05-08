7782488255401b34fdfb740c35e8ffc65f781f14
toolmanp <toolmanp@outlook.com>
2022-05-08 20:09:38 +0800

    Implement expression evaluation.

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -1,19 +1,20 @@
-#include <isa.h>
 #include <cpu/cpu.h>
-#include <readline/readline.h>
+#include <isa.h>
 #include <readline/history.h>
+#include <readline/readline.h>
 
 #include "common.h"
-#include <memory/paddr.h>
 #include "sdb.h"
+#include <memory/paddr.h>
 
 static int is_batch_mode = false;
 
 void init_regex();
 void init_wp_pool();
 
-/* We use the `readline' library to provide more flexibility to read from stdin. */
-static char* rl_gets() {
+/* We use the `readline' library to provide more flexibility to read from stdin.
+ */
+static char *rl_gets() {
   static char *line_read = NULL;
 
   if (line_read) {
@@ -35,80 +36,97 @@ static int cmd_c(char *args) {
   return 0;
 }
 
-
-static int cmd_q(char *args) {
-  return -1;
-}
+static int cmd_q(char *args) { return -1; }
 
 static int cmd_help(char *args);
 
-static int cmd_si(char *args){
-  int steps=strtol(args,NULL,10);
+static int cmd_si(char *args) {
+  int steps = strtol(args, NULL, 10);
   cpu_exec(steps);
   return 0;
 }
 
-static int cmd_info(char *args){
-  
-  if(args==NULL){
-    printf("Please pass through subcommands");
+static int cmd_info(char *args) {
+
+  if (args == NULL) {
+    printf("Please pass through subcommands.\n");
     return 0;
   }
-  
-  char *cmd=strtok(args," ");
-  if(cmd[0]=='r'){
+
+  char *cmd = strtok(args, " ");
+  if (cmd[0] == 'r') {
     isa_reg_display();
-  }else{
-    printf("INFO only supports subcommands(r/w)\n");
+  } else {
+    printf("INFO only supports subcommands(r/w).\n");
   }
   return 0;
 }
 
-static int cmd_x(char *args){
+static int cmd_x(char *args) {
 
-  char *args_end = args+strlen(args);
+  char *args_end = args + strlen(args);
 
-  if(args==NULL){
-    printf("Not Enough Arguments\n");
-    return 0;
-  }
-
-  char *Bytes = strtok(args," ");
-  int bytes = strtol(Bytes,NULL,10);
-  args = args + strlen(Bytes) + 1;
-
-  if(args>=args_end){
+  if (args == NULL) {
     printf("Not Enough Arguments.\n");
     return 0;
   }
 
-  char *Addr = strtok(args," ");
-  word_t addr = strtoll(Addr,NULL,16);
-  
+  char *Bytes = strtok(args, " ");
+  int bytes = strtol(Bytes, NULL, 10);
+  args = args + strlen(Bytes) + 1;
+
+  if (args >= args_end) {
+    printf("Not Enough Arguments.\n");
+    return 0;
+  }
+
+  char *Addr = strtok(args, " ");
+  word_t addr = strtoll(Addr, NULL, 16);
+
   printf("---------MEMORY----------\n");
-  printf("Total Bytes: %d\n",bytes);
-  printf("Starting Addr: 0x%016lx\n",addr);
+  printf("Total Bytes: %d\n", bytes);
+  printf("Starting Addr: 0x%016lx\n", addr);
   printf("Mem: ");
-  for(int i=0;i<bytes;i++)
-      printf("0x%02x ",*guest_to_host(addr+i));
+  for (int i = 0; i < bytes; i++)
+    printf("0x%02x ", *guest_to_host(addr + i));
   printf("\n");
   printf("-----------END-----------\n");
+  return 0;
+}
+
+int cmd_p(char *args){
+
+  if(args==NULL){
+    printf("Please provide an expression.\n");
+    return 0;
+  }
+
+  bool success;
+  word_t val = expr(args,&success);
+
+  if (success)
+  {
+    printf("0x%016lx\n",val);
+  }else{
+    printf("Error: Regex no match.\n");
+  }
   return 0;
 }
 
 static struct {
   const char *name;
   const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display informations about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "Step Into", cmd_si},
-  { "info", "Info status (r/w)", cmd_info},
-  { "x", "Print N bytes starting from the address", cmd_x}
+  int (*handler)(char *);
+} cmd_table[] = {
+    {"help", "Display informations about all supported commands", cmd_help},
+    {"c", "Continue the execution of the program", cmd_c},
+    {"q", "Exit NEMU", cmd_q},
+    {"si", "Step Into", cmd_si},
+    {"info", "Info status (r/w)", cmd_info},
+    {"x", "Print N bytes starting from the address", cmd_x},
+    {"p", "Eval the result of the expression.",cmd_p},
 
-  /* TODO: Add more commands */
+    /* TODO: Add more commands */
 
 };
 
@@ -121,12 +139,11 @@ static int cmd_help(char *args) {
 
   if (arg == NULL) {
     /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
+    for (i = 0; i < NR_CMD; i++) {
       printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
+  } else {
+    for (i = 0; i < NR_CMD; i++) {
       if (strcmp(arg, cmd_table[i].name) == 0) {
         printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
         return 0;
@@ -137,9 +154,7 @@ static int cmd_help(char *args) {
   return 0;
 }
 
-void sdb_set_batch_mode() {
-  is_batch_mode = true;
-}
+void sdb_set_batch_mode() { is_batch_mode = true; }
 
 void sdb_mainloop() {
   if (is_batch_mode) {
@@ -147,12 +162,14 @@ void sdb_mainloop() {
     return;
   }
 
-  for (char *str; (str = rl_gets()) != NULL; ) {
+  for (char *str; (str = rl_gets()) != NULL;) {
     char *str_end = str + strlen(str);
 
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
-    if (cmd == NULL) { continue; }
+    if (cmd == NULL) {
+      continue;
+    }
 
     /* treat the remaining string as the arguments,
      * which may need further parsing
@@ -168,14 +185,18 @@ void sdb_mainloop() {
 #endif
 
     int i;
-    for (i = 0; i < NR_CMD; i ++) {
+    for (i = 0; i < NR_CMD; i++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) { return; }
+        if (cmd_table[i].handler(args) < 0) {
+          return;
+        }
         break;
       }
     }
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+    if (i == NR_CMD) {
+      printf("Unknown command '%s'\n", cmd);
+    }
   }
 }
 

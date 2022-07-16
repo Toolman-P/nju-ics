c536928d6a6c42a36991c775363a88fd40d18319
toolmanp <toolmanp@outlook.com>
2022-07-16 20:33:24 +0800

    Feat: audio(pretty laggy)

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,33 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+static int bufsize = 0;
+
 void __am_audio_init() {
+  bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  printf("%d\n",bufsize);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = bufsize;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR,ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR,ctrl->samples);
+  outl(AUDIO_INIT_ADDR,1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  uint8_t *start = ctl->buf.start,
+          *end   = ctl->buf.end;
+  for(uint8_t i=0;start+i<end;i++)
+    outb(AUDIO_SBUF_ADDR+i,*(start+i));
+  return;
 }

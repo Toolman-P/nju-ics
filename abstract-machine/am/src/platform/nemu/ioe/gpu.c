f6638e7391c458e67e8ebb830d0d34ed8397b46a
toolmanp <toolmanp@outlook.com>
2022-07-20 23:18:20 +0800

    Opti: use direct memory access

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,12 +7,6 @@
 static uint32_t vga_w = 0;
 static uint32_t vga_h = 0;
 
-#define PIXEL(i,j,ctl) \
-  *(uint32_t *)(ctl->pixels+4*(j*ctl->w+i))
-
-#define VMEM(i,j,ctl) \
-  (FB_ADDR+4*(vga_w*(ctl->y+j)+ctl->x+i))
-
 void __am_gpu_init() {
   
 }
@@ -31,18 +25,18 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  int w=ctl->w,h=ctl->h,x=ctl->x,y=ctl->y;
   
-  if (ctl->sync) {
+  uint32_t *fb = (uint32_t *)FB_ADDR;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  for(int i=0;i<w;i++)
+    for(int j=0;j<h;j++)
+      fb[(y+j)*vga_w+x+i]=pixels[j*w+i];  
+
+  if (ctl->sync)
     outl(SYNC_ADDR, 1);
-  }else{
-    int w=ctl->w,h=ctl->h;
-    if(w==0||h==0)
-      return;
-    for(int i=0;i<w;i++)
-      for(int j=0;j<h;j++){
-        outl(VMEM(i,j,ctl),PIXEL(i,j,ctl));
-      }  
-  } 
+     
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {

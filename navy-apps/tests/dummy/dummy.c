a81d127081d944ab7ef4e425ff26f0fa54510bc6
toolmanp <toolmanp@outlook.com>
2022-08-07 12:04:02 +0800

    Feat(tests): Update some test cases

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
@@ -8,5 +10,7 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
+  printf("Hello %d\n",2);
+  while(1);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
